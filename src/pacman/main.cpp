03415ebcf6ef034ff9721b23d6bc69cc2b5660bf
weinha_l <loic.weinhard@epitech.eu>
2016-03-07 18:39:04 +0100

    [ARCHI] - Modif du Makefile général et rajout de mains pour les algos de jeux

---
+++ src/pacman/main.cpp
@@ -0,0 +1,14 @@
+//
+// main.cpp for  in /home/weinha_l/Semestre_4/mouillette_arcade/src/pacman
+// 
+// Made by Loïc Weinhard
+// Login   <weinha_l@epitech.net>
+// 
+// Started on  Mon Mar  7 17:47:11 2016 Loïc Weinhard
+// Last update Mon Mar  7 17:47:22 2016 Loïc Weinhard
+//
+
+int	main()
+{
+  return (0);
+}
