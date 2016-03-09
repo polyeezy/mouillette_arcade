4ffb0d44728e2e23fab3d81e9456f0ab7fca245b
weinha_l <loic.weinhard@epitech.eu>
2016-03-09 12:18:40 +0100

    [ENV] - Rename classe Pacman en PEntity

--- include/pacman/PEntity.hh
+++ include/pacman/PEntity.hh
@@ -5,23 +5,23 @@
 // Login   <weinha_l@epitech.net>
 // 
 // Started on  Tue Mar  8 16:54:19 2016 Loïc Weinhard
-// Last update Tue Mar  8 17:48:48 2016 Loïc Weinhard
+// Last update Wed Mar  9 12:15:50 2016 Loïc Weinhard
 //
 
-#ifndef PACMAN_HH_
-# define PACMAN_HH_
+#ifndef PENTITY_HH_
+# define PENTITY_HH_
 
 # include <Protocol.hpp>
 # include "AEntity.hh"
 
-class	Pacman : public AEntity
+class	PEntity : public AEntity
 {
 private:
   bool	_god;
 
 public:
-  Pacman(const arcade::Size, const arcade::Position, const float);
-  virtual ~Pacman();
+  PEntity(const arcade::Size, const arcade::Position, const float);
+  virtual ~PEntity();
   bool		getGod() const;
   void		setGod(const bool);
 };
---
+++ src/pacman/PEntity.cpp
@@ -0,0 +1,30 @@
+//
+// PEntity.cpp for  in /home/weinha_l/Semestre_4/mouillette_arcade/src/pacman
+// 
+// Made by Loïc Weinhard
+// Login   <weinha_l@epitech.net>
+// 
+// Started on  Tue Mar  8 16:59:42 2016 Loïc Weinhard
+// Last update Wed Mar  9 12:17:57 2016 Loïc Weinhard
+//
+
+#include "PEntity.hh"
+
+PEntity::PEntity(const arcade::Size s, const arcade::Position p, const float sp) : AEntity(s, p, sp)
+{
+  _god = false;
+}
+
+PEntity::~PEntity()
+{
+}
+
+bool	PEntity::getGod() const
+{
+  return _god;
+}
+
+void	PEntity::setGod(const bool god)
+{
+  _god = god;
+}
